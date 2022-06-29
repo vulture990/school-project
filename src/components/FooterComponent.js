fe07d6bae1cd8d85a0847ea94d22c3ce98477796
vul <mohamedsali602@live.com>
2022-06-29 23:15:48 +0100

    added

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+    return (
+        <div>
+            <footer className = "footer">
+                Ensias @ 2022
+            </footer>
+        </div>
+    )
+}
+
+export default FooterComponent
